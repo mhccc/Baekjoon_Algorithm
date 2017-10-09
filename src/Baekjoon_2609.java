05391b6bd080381235c9da22dfed0dd503fb37b6
mhccc <miinhyuk94@naver.com>
2017-10-09 23:50:49 +0900

    Modify 2609

--- src/Baekjoon_2609.java
+++ src/Baekjoon_2609.java
@@ -1,12 +1,13 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Baekjoon_2609 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] num = br.readLine().split(" ");
-		int x = Integer.parseInt(num[0]), y = Integer.parseInt(num[1]);
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int x = Integer.parseInt(st.nextToken()), y = Integer.parseInt(st.nextToken());
 		
 		int n = gcd(x, y);
 		System.out.println(n);
