076d659c991535784f533e0719bb7f92e791ff1b
mhccc <mhccc@naver.com>
2018-02-06 20:46:28 +0900

    Add 10869

---
+++ src/Baekjoon_10869.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Baekjoon_10869 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int a = input.nextInt();
+		int b = input.nextInt();
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+	}
+}
